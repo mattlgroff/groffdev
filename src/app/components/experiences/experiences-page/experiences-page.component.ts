e02881d1968e8c36d46eb427266a532ee747f3ac
Matthew Groff <matthewgroff@matthews-mbp.attlocal.net>
2019-06-02 20:26:36 -0500

    Revert "Update security vulnerability packages & Update Experiences"
    This reverts commit 396fdcbe4edee97931d4a6c865fba79021422561.

--- src/app/components/experiences/experiences-page/experiences-page.component.ts
+++ src/app/components/experiences/experiences-page/experiences-page.component.ts
@@ -13,10 +13,16 @@ interface Experience {
 export class ExperiencesPageComponent {
   public experiences: Experience[] = [
     {
-      header: 'Intrepid (Accenture Digital Products) - Web Developer',
-      date: 'June 2018 - Present Day!',
+      header: 'Intrepid - Web Developer',
+      date: 'September 2018 - Present Day!',
       content:
         'Intrepid hired me on as a Web Developer. I work on tons of different projects and work with great people at the Houston Innovation Hub.',
+    },
+    {
+      header: 'Intrepid - Web Developer Apprentice',
+      date: 'June 2018',
+      content:
+        'I began at Intrepid in June 2018 as a Web Developer Apprentice.',
     },
     {
       header: 'UC San Diego Extension Coding Bootcamp',
